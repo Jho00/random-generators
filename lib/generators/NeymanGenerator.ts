92682d3091b0d1ad9a8dfd4ef2923ce27c38887e
Jho00 <akutin_artem@mail.ru>
2019-12-03 23:18:49 +0400

    fix

--- lib/generators/NeymanGenerator.ts
+++ lib/generators/NeymanGenerator.ts
@@ -2,10 +2,10 @@ import IGenerator from "../common/IGenerator";
 
 /**
  * Это говнина сраная. Тут можно в качестве начального значения брать указатель мышки или еще что-то, но рано или поздно
- * эта срань все равно будет давать постоянный 0. Метод древний, как говно мамонта, и не рабочий, как Тю после 3 литра пива.
+ * эта срань все равно будет давать периодичные значения. Метод древний, как говно мамонта, и не рабочий, как Тю после 3 литра пива.
  */
 class NeymanGenerator implements IGenerator {
-    private _nextNumber: number = 3466;
+    private _nextNumber: number = 9889;
     generate(): number {
         const result = this._nextNumber / 10000;
         this._nextNumber = this.getNewNextNumber();
@@ -14,7 +14,7 @@ class NeymanGenerator implements IGenerator {
 
     private getNewNextNumber(): number {
         const sq = this._nextNumber * this._nextNumber;
-        return Math.floor( ((sq * sq) % 1000000 ) / 100);
+        return Math.floor( (sq % 1000000 ) / 100);
     }
 
 }
